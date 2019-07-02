29128ab26b8b4733d1065ec4014946e510a50814
4bhishekKasam <konnurabhishek@gmail.com>
2019-07-02 20:29:50 +0530

    first commit

---
+++ src/reducers/productReducer.js
@@ -0,0 +1,35 @@
+import {
+  FETCH_PRODUCTS,
+  FILTER_PRODUCTS_BY_SIZE,
+  ORDER_PRODUCTS_BY_PRICE
+} from "../actions/types";
+
+const initialState = { items: [], filteredProducts: [], size: "" };
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_PRODUCTS:
+      return {
+        ...state,
+        items: action.payload,
+        filteredProducts: action.payload
+      };
+
+    case FILTER_PRODUCTS_BY_SIZE:
+      return {
+        ...state,
+        filteredProducts: action.payload.items,
+        size: action.payload.size
+      };
+
+    case ORDER_PRODUCTS_BY_PRICE:
+      return {
+        ...state,
+        filteredProducts: action.payload.items,
+        sort: action.payload.sort
+      };
+
+    default:
+      return state;
+  }
+}
